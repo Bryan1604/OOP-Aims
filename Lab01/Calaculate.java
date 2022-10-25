6be93356f3cea155b155c572b309b417548e8b57
vdluong_20205099_OOP <luong.vd205099@sis.hust.edu.vn>
2022-10-25 18:07:56 +0700

    add comment

--- Lab01/Calaculate.java
+++ Lab01/Calaculate.java
@@ -8,19 +8,22 @@ public class Calculate {
         String strNum1, strNum2; // khai báo hai xau de chua so nguoi dung nhap
         Double sum, diff, prod, qout; // khai báo 4 xâu để chứa kết quả cua phép tính
 
+        // nhập vào màn hình log số thứ nhất
         strNum1 = JOptionPane.showInputDialog(null, "enter the first number: ", JOptionPane.INFORMATION_MESSAGE);
-        double num1 = Double.parseDouble(strNum1);
+        double num1 = Double.parseDouble(strNum1); // convert số thứ nhất từ kiểu string sang kiểu double
 
+        // nhập vào màn hình log số thứ hai
         strNum2 = JOptionPane.showInputDialog(null, "enter the second number: ", JOptionPane.INFORMATION_MESSAGE);
-        double num2 = Double.parseDouble(strNum2);
+        double num2 = Double.parseDouble(strNum2); // convert số thứ hai từ kiểu string sang kiểu double
 
         sum = num1 + num2;
-        System.out.println("Sum of 2 numbers: " + sum);
+        System.out.println("Sum of 2 numbers: " + sum); // in tổng
         diff = num1 - num2;
-        System.out.println("Diffirence of 2 numbers: " + diff);
+        System.out.println("Diffirence of 2 numbers: " + diff); // in hiệu 2 số
         prod = num1 * num2;
-        System.out.println("Product of 2 numbers: " + prod);
-        if (num2 != 0) {
+        System.out.println("Product of 2 numbers: " + prod); // in tích 2 số
+        if (num2 != 0) { // kiểm tra điều kiện của số thứ 2 : nếu số thứ 2 khác không thì kết quả trả về
+                         // đúng, nếu = 0 thì in ra "can not define "
             qout = num1 / num2;
             System.out.println("Division of 2 numbers: " + qout);
         } else {
