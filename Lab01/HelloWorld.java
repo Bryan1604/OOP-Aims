5b63034d8c23ffe6feb79d5d92c4cc08cbad5394
vdluong_20205099_OOP <luong.vd205099@sis.hust.edu.vn>
2022-10-25 18:38:55 +0700

    fix the name files

--- Lab01/HelloWorld.java
+++ Lab01/HelloWorld.java
@@ -1,9 +1,9 @@
 //Example1: Hellowolrd
 // Vũ Đức Lương - 20205099
 
-public class Hellowolrd{
-    public static void main(String[] args){
-        System.out.println("Xin chao cac ban!");   // in xau 
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Xin chao cac ban!"); // in xau
         System.out.println("Hello World");
     } // end of main method
 }
