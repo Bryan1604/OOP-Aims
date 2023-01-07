5f90b052912293f4fe09da9c790db5fa0556d97c
Bryan1604 <luongvuhs@gmail.com>
2023-01-07 13:57:06 +0700

    update

--- Lab02/Aims_project_v2/src/hust/soict/dsai/aims/screen/CartScreen.java
+++ Lab02/Aims_project_v2/src/hust/soict/dsai/aims/screen/CartScreen.java
@@ -1,7 +1,10 @@
 package hust.soict.dsai.aims.screen;
 
 import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.media.Book;
+import hust.soict.dsai.aims.media.CompactDisc;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Track;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
@@ -12,6 +15,8 @@ import javafx.scene.Scene;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static javafx.application.Application.launch;
 
@@ -35,7 +40,7 @@ public class CartScreen extends JFrame {
                     CartScreenController controller = new CartScreenController(cart);
                     loader.setController(controller);
                     Parent root = loader.load();
-                    fxPanel.setScene(new Scene(root));
+                    fxPanel.setScene(new Scene(root,1024,748));
                 }catch (IOException e){
                     e.printStackTrace();
                 }
@@ -44,9 +49,27 @@ public class CartScreen extends JFrame {
     }
 
     public static void main(String[] args){
+        List<Track> tracks = new ArrayList<Track>();
+        Track t1,t2,t3;
+        t1 = new Track("Track1", 1);
+        t2 = new Track("Track2", 2);
+        t3 = new Track("Track3", 3);
+        tracks.add(t1);
+        tracks.add(t2);
+        tracks.add(t3);
+
+        CompactDisc cd = new CompactDisc(1,"cd","a",122f,"vdl",32,"vdl",tracks);
+        DigitalVideoDisc vdv = new DigitalVideoDisc(2,"hell","englsh","rerer",23,34f);
+
+        List<String> author = new ArrayList<String>();
+        author.add("jame");
+        author.add("Luong");
+        Book book = new Book(3,"KHKE","HISTPR",23f,author);
         Cart cart = new Cart();
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc(1,"English","category1","luong",12,13);
-        cart.addMedia(dvd1);
+        cart.addMedia(cd);
+        cart.addMedia(vdv);
+        cart.addMedia(book);
+
         new CartScreen(cart);
         //launch(args);
     }
