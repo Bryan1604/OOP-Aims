3662b24d4e9f27aa1ff31f11252f7e0ed80531cd
Bryan1604 <luongvuhs@gmail.com>
2023-01-07 20:57:33 +0700

    update

--- Lab02/GUIProject/src/hust/soict/dsai/javafx/Painter.java
+++ Lab02/GUIProject/src/hust/soict/dsai/javafx/Painter.java
@@ -1,2 +1,24 @@
-package hust.soict.dsai.javafx;public class Painter {
+package hust.soict.dsai.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Scanner;
+
+public class Painter extends Application {
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Painter.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("VDL-Painter");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
--- Lab02/GUIProject/src/hust/soict/dsai/javafx/PainterController.java
+++ Lab02/GUIProject/src/hust/soict/dsai/javafx/PainterController.java
@@ -1,2 +1,24 @@
-package hust.soict.dsai.javafx;public class PainterController {
+package hust.soict.dsai.javafx;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+public class PainterController {
+    private Pane drawingAreaPane;
+    void clearButtonPressed(ActionEvent event){
+        //implement clear of canvas
+        drawingAreaPane.getChildren().clear();
+    }
+
+    void drawingAreaMouseDragged(MouseEvent event){
+        //implement drawing
+        Circle newCircle = new Circle(event.getX(),event.getY(),
+                4, Color.BLACK);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
 }
